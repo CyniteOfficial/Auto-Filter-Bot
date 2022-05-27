30a26a7456a3e131891d53f9310fe04010ae926f
Cyniteb <106390874+Cyniteb@users.noreply.github.com>
2022-05-27 20:19:34 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -93,7 +93,7 @@ def get_reply_markup(query):
         [
             InlineKeyboardButton('♻️ 𝗦𝗲𝗮𝗿𝗰𝗵 𝗔𝗴𝗮𝗶𝗻 ♻️', switch_inline_query_current_chat=query)
         ],[
-            InlineKeyboardButton('♥️ 𝗖𝗵𝗮𝗻𝗻𝗲𝗹 ♥️', url='https://t.me/CYNITEBOTS')
+            InlineKeyboardButton('♥️𝗖𝗵𝗮𝗻𝗻𝗲𝗹♥️', url='https://t.me/CYNITEBOTS')
         ],[
             InlineKeyboardButton('🥳 υѕє ιи α gяουρ 👥', url='http://t.me/ZsearcherBot?startgroup=true')
         ]
